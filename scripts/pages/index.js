31cc4318567d5f18ec576da91f2880f539c48275
kevinmbongo <mbongo.kevin.pro@gmail.com>
2023-12-15 15:43:50 +0100

    refactor(index): create getPhototgrapherJSON and enhance import

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,19 +1,5 @@
-const JSON_PATH = "./data/photographers.json";
-
-async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  try {
-    const repons = await fetch(JSON_PATH);
-
-    const photographerArray = await repons.json();
-    // et bien retourner le tableau photographers seulement une fois récupéré
-    return { ...photographerArray };
-  } catch (error) {
-    console.error("Erreur lors de la récupération des photographes:", error);
-    throw error;
-  }
-}
+import { photographerTemplate } from "../templates/photographerCard.js";
+import { getPhotographers } from "../utils/getPhotographersJSON.js";
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
